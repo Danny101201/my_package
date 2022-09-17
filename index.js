1124c5e5820e3aeac7d8861ec459c144177043e2
Danny <hiunji64@gmail.com>
2022-09-17 20:25:11 +0800

    dsf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("Hello from our very own npm package.")
 
 const sayMyName = name => console.log(`Your name is ${name}.`)
+const showage = age => console.log(age)
 
-module.exports = {sayMyName}
+module.exports = { sayMyName, showage }
