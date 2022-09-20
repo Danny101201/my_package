59a7303ab6e0ff9f6d4a9bc3262fc3363b57a3ef
Danny <hiunji64@gmail.com>
2022-09-20 10:18:47 +0800

    feat: add version4

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const sayMyName = name => console.log(`Your name is ${name}.`)
 const showage = age => console.log(age)
 const test3 = age => console.log(age)
 const test4 = () => console.log('test4')
+const test5 = () => console.log('test5')
 
 
-module.exports = { sayMyName, showage, test3, test4 }
+module.exports = { sayMyName, showage, test3, test4, test5 }
