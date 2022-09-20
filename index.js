dd93c54857d6087316f9dfd7f86682b8bf822610
Danny <hiunji64@gmail.com>
2022-09-20 10:16:08 +0800

    feat: add version4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("Hello from our very own npm package.")
 const sayMyName = name => console.log(`Your name is ${name}.`)
 const showage = age => console.log(age)
 const test3 = age => console.log(age)
+const test4 = () => console.log('test4')
 
 
-module.exports = { sayMyName, showage, test3 }
+module.exports = { sayMyName, showage, test3, test4 }
